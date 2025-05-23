3172defdded67cd0cd36b635df4eab0b08245de5
MonarchRyuzaki <shivamganguly2004@gmail.com>
2025-05-23 13:33:11 +0530

    Plan For next few days is made

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,23 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <LandingPage />,
-      }
-    ]
+      },
+      {
+        path: "/login", // use URL params for Donor and NGO login
+      },
+      {
+        path: "/register", // use URL params for Donor and NGO register
+      },
+      {
+        path: "/donations", // Here all the donation listing will be shown along with the filter options. The filter slected will be saved in the URL params
+      },
+      {
+        path: "/donation/:id", // Here the donation details will be shown
+      },
+      {
+        path: "/donation/:id/edit", // Here will be the edit donation form. The form will be prefilled with the donation details. It will be accessible to onlt the creator of the donation. Same for delete
+      },
+    ],
   },
 ]);
 export default function App() {
